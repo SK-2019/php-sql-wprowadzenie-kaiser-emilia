f8e47930dd4b73fe9dc7c106d4a5058a7fab63ce
kaiser-emilia <73523733+kaiser-emilia@users.noreply.github.com>
2020-10-30 14:34:16 +0100

    Update index.php

--- index.php
+++ index.php
@@ -31,8 +31,39 @@ $result=$conn->query('SELECT * FROM pracownicy where imie like "%a"');
         echo("</table>");
 
 echo("<h2>Zadanie 3</h2>");
-echo("<li>sql");
 $sql = "SELECT * FROM pracownicy where imie like '%a' and dzial = 2";
+$conn = new mysqli("remotemysql.com","gQvQ0qIoDC","4HAPys5ynL","gQvQ0qIoDC");
+$result=$conn->query($sql);
+        echo("<table border=1>");
+        echo("<th>id</th>");
+        echo("<th>imie</th>");
+        echo("<th>dzial</th>");
+        echo("<th>zarobki</th>");
+            while($row=$result->fetch_assoc()) {
+                echo("<tr>");
+                    echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td>");
+                echo("</tr>");
+            }
+        echo("</table>");
+
+echo("<h2>Zadanie 4</h2>");
+$sql = "SELECT * FROM pracownicy where imie like '%a' and dzial = 2 or dzial =3";
+$conn = new mysqli("remotemysql.com","gQvQ0qIoDC","4HAPys5ynL","gQvQ0qIoDC");
+$result=$conn->query($sql);
+        echo("<table border=1>");
+        echo("<th>id</th>");
+        echo("<th>imie</th>");
+        echo("<th>dzial</th>");
+        echo("<th>zarobki</th>");
+            while($row=$result->fetch_assoc()) {
+                echo("<tr>");
+                    echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td>");
+                echo("</tr>");
+            }
+        echo("</table>");
+
+echo("<h2>Zadanie 5</h2>");
+$sql = "SELECT * FROM pracownicy where imie like '%a' and dzial = 1";
 $conn = new mysqli("remotemysql.com","gQvQ0qIoDC","4HAPys5ynL","gQvQ0qIoDC");
 $result=$conn->query($sql);
         echo("<table border=1>");
