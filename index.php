e2b12e5eef40475325cab8d827fdc2b084cbad41
kaiser-emilia <73523733+kaiser-emilia@users.noreply.github.com>
2020-10-30 15:01:05 +0100

    Update index.php

--- index.php
+++ index.php
@@ -35,7 +35,7 @@ $result=$conn->query($sql);
         echo("</table>");
 
 echo("<h2>Zadanie 3</h2>");
-$sql = ("SELECT * FROM pracownicy where imie like '%a' and dzial = 2");
+$sql = ("SELECT * FROM pracownicy where (imie like '%a') and (dzial = 2)");
 echo("<h2>".$sql."</h2>");
 $conn = new mysqli("remotemysql.com","gQvQ0qIoDC","4HAPys5ynL","gQvQ0qIoDC");
 $result=$conn->query($sql);
