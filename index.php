50bf7732fbf0f5c78b15185f58dbad4d0bd6da2f
kaiser-emilia <73523733+kaiser-emilia@users.noreply.github.com>
2020-10-30 14:59:21 +0100

    Update index.php

--- index.php
+++ index.php
@@ -18,8 +18,10 @@ $result=$conn->query($sql);
         echo("</table>");
 
 echo("<h2>Zadanie 2</h2>");
+$sql = ("SELECT * FROM pracownicy where imie like '%a'");
+echo("<h2>".$sql."</h2>");
 $conn = new mysqli("remotemysql.com","gQvQ0qIoDC","4HAPys5ynL","gQvQ0qIoDC");
-$result=$conn->query('SELECT * FROM pracownicy where imie like "%a"');
+$result=$conn->query($sql);
         echo("<table border=1>");
         echo("<th>id</th>");
         echo("<th>imie</th>");
@@ -33,7 +35,8 @@ $result=$conn->query('SELECT * FROM pracownicy where imie like "%a"');
         echo("</table>");
 
 echo("<h2>Zadanie 3</h2>");
-$sql = "SELECT * FROM pracownicy where imie like '%a' and dzial = 2";
+$sql = ("SELECT * FROM pracownicy where imie like '%a' and dzial = 2");
+echo("<h2>".$sql."</h2>");
 $conn = new mysqli("remotemysql.com","gQvQ0qIoDC","4HAPys5ynL","gQvQ0qIoDC");
 $result=$conn->query($sql);
         echo("<table border=1>");
@@ -49,7 +52,8 @@ $result=$conn->query($sql);
         echo("</table>");
 
 echo("<h2>Zadanie 4</h2>");
-$sql = "SELECT * FROM pracownicy where imie like '%a' and dzial = 2 or dzial =3";
+$sql = ("SELECT * FROM pracownicy where imie like '%a' and dzial = 2 or dzial =3");
+echo("<h2>".$sql."</h2>");
 $conn = new mysqli("remotemysql.com","gQvQ0qIoDC","4HAPys5ynL","gQvQ0qIoDC");
 $result=$conn->query($sql);
         echo("<table border=1>");
@@ -65,7 +69,8 @@ $result=$conn->query($sql);
         echo("</table>");
 
 echo("<h2>Zadanie 5</h2>");
-$sql = "SELECT * FROM pracownicy where imie like '%a' and dzial = 1";
+echo("<h2>".$sql."</h2>");
+$sql = ("SELECT * FROM pracownicy where imie like '%a' and dzial = 1");
 $conn = new mysqli("remotemysql.com","gQvQ0qIoDC","4HAPys5ynL","gQvQ0qIoDC");
 $result=$conn->query($sql);
         echo("<table border=1>");
