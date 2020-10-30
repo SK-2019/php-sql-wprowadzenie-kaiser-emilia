59dd58afc33c120eb16205f914cdc526c2b1caba
kaiser-emilia <73523733+kaiser-emilia@users.noreply.github.com>
2020-10-30 14:23:14 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,38 @@
 <?php
 echo("<h1>Emilia Kaiser</h1>");
+echo("<h2>Zadanie 1</h2>");
 $conn = new mysqli("remotemysql.com","gQvQ0qIoDC","4HAPys5ynL","gQvQ0qIoDC");
 $result=$conn->query('SELECT * FROM pracownicy');
+        echo("<table border=1>");
+        echo("<th>id</th>");
+        echo("<th>imie</th>");
+        echo("<th>dzial</th>");
+        echo("<th>zarobki</th>");
+            while($row=$result->fetch_assoc()) {
+                echo("<tr>");
+                    echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td>");
+                echo("</tr>");
+            }
+        echo("</table>");
+
+echo("<h2>Zadanie 2</h2>");
+$conn = new mysqli("remotemysql.com","gQvQ0qIoDC","4HAPys5ynL","gQvQ0qIoDC");
+$result=$conn->query('SELECT * FROM pracownicy where imie like "%a"');
+        echo("<table border=1>");
+        echo("<th>id</th>");
+        echo("<th>imie</th>");
+        echo("<th>dzial</th>");
+        echo("<th>zarobki</th>");
+            while($row=$result->fetch_assoc()) {
+                echo("<tr>");
+                    echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td>");
+                echo("</tr>");
+            }
+        echo("</table>");
+
+echo("<h2>Zadanie 3</h2>");
+$conn = new mysqli("remotemysql.com","gQvQ0qIoDC","4HAPys5ynL","gQvQ0qIoDC");
+$result=$conn->query('SELECT * FROM pracownicy where imie like "%a" and dzial = 2');
         echo("<table border=1>");
         echo("<th>id</th>");
         echo("<th>imie</th>");
