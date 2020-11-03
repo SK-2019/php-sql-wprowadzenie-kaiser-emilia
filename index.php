9ee7239bf17713991e66fadc13e6ab772d72bf0d
kaiser-emilia <73523733+kaiser-emilia@users.noreply.github.com>
2020-11-03 10:28:57 +0100

    Update index.php

--- index.php
+++ index.php
@@ -101,21 +101,21 @@
                         echo("</table>");
         
                   echo("<h2>Zadanie 6</h2>");
-                  $sql = ("SELECT avg(zarobki) as suma_zarobkow FROM pracownicy, organizacja where dzial=id_org");
+                  $sql = ("SELECT avg(zarobki) as srednia_zarobkow FROM pracownicy, organizacja where dzial=id_org");
                   echo("<h2>".$sql."</h2>");
                   $conn = new mysqli("remotemysql.com","gQvQ0qIoDC","4HAPys5ynL","gQvQ0qIoDC");
                   $result=$conn->query($sql);
                          echo("<table border=1>");
-                         echo("<th>suma_zarobkow</th>");
+                         echo("<th>srednia_zarobkow</th>");
                         
                          while($row=$result->fetch_assoc()) {
                                   echo("<tr>");
-                                        echo("<td>".$row["suma_zarobkow"]."</td>");
+                                        echo("<td>".$row["srednia_zarobkow"]."</td>");
                                    echo("</tr>");
                                     }
                          echo("</table>");
         
-                   echo("<h2>Zadanie 6</h2>");
+                   echo("<h2>Zadanie 7</h2>");
                   $sql = ("SELECT count(imie) as liczba_kobiet FROM pracownicy, organizacja where dzial=id_org and imie like '%a'");
                   echo("<h2>".$sql."</h2>");
                   $conn = new mysqli("remotemysql.com","gQvQ0qIoDC","4HAPys5ynL","gQvQ0qIoDC");
@@ -130,7 +130,20 @@
                                     }
                          echo("</table>");      
         
-
+                  echo("<h2>Zadanie 8</h2>");
+                  $sql = ("SELECT sum(zarobki) as suma_zarobkow FROM pracownicy, organizacja where dzial=id_org");
+                  echo("<h2>".$sql."</h2>");
+                  $conn = new mysqli("remotemysql.com","gQvQ0qIoDC","4HAPys5ynL","gQvQ0qIoDC");
+                  $result=$conn->query($sql);
+                         echo("<table border=1>");
+                         echo("<th>suma_zarobkow</th>");
+                        
+                         while($row=$result->fetch_assoc()) {
+                                  echo("<tr>");
+                                        echo("<td>".$row["suma_zarobkow"]."</td>");
+                                   echo("</tr>");
+                                    }
+                         echo("</table>");
 
         ?>
 </html>
