da0a7bb9c482fbdca4d82c5cd24ec43fbe0d82fe
kaiser-emilia <73523733+kaiser-emilia@users.noreply.github.com>
2020-11-03 09:10:07 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,92 +1,90 @@
 <!DOCTYPE html>
 <html>
-<head>
-<link rel="stylesheet" href="style.css">
-</head>
-<body>
-<?php
-echo("<h1>Emilia Kaiser</h1>");
-echo("<h2>Zadanie 1</h2>");
-$sql = ("SELECT * FROM pracownicy");
-echo("<h2>".$sql."</h2>");
-$conn = new mysqli("remotemysql.com","gQvQ0qIoDC","4HAPys5ynL","gQvQ0qIoDC");
-$result=$conn->query($sql);
-include("connect.php");
-        echo("<table border=1>");
-        echo("<th>id</th>");
-        echo("<th>imie</th>");
-        echo("<th>dzial</th>");
-        echo("<th>zarobki</th>");
-            while($row=$result->fetch_assoc()) {
-                echo("<tr>");
-                    echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td>");
-                echo("</tr>");
-            }
-        echo("</table>");
-echo("<h2>Zadanie 2</h2>");
-$sql = ("SELECT * FROM pracownicy where imie like '%a'");
-echo("<h2>".$sql."</h2>");
-$conn = new mysqli("remotemysql.com","gQvQ0qIoDC","4HAPys5ynL","gQvQ0qIoDC");
-$result=$conn->query($sql);
-        echo("<table border=1>");
-        echo("<th>id</th>");
-        echo("<th>imie</th>");
-        echo("<th>dzial</th>");
-        echo("<th>zarobki</th>");
-            while($row=$result->fetch_assoc()) {
-                echo("<tr>");
-                    echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td>");
-                echo("</tr>");
-            }
-        echo("</table>");
-echo("<h2>Zadanie 3</h2>");
-$sql = ("SELECT * FROM pracownicy where (imie like '%a') and (dzial = 2)");
-echo("<h2>".$sql."</h2>");
-$conn = new mysqli("remotemysql.com","gQvQ0qIoDC","4HAPys5ynL","gQvQ0qIoDC");
-$result=$conn->query($sql);
-        echo("<table border=1>");
-        echo("<th>id</th>");
-        echo("<th>imie</th>");
-        echo("<th>dzial</th>");
-        echo("<th>zarobki</th>");
-            while($row=$result->fetch_assoc()) {
-                echo("<tr>");
-                    echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td>");
-                echo("</tr>");
-            }
-        echo("</table>");
-echo("<h2>Zadanie 4</h2>");
-$sql = ("SELECT * FROM pracownicy where (imie like '%a') and (dzial = 2 or dzial =3)");
-echo("<h2>".$sql."</h2>");
-$conn = new mysqli("remotemysql.com","gQvQ0qIoDC","4HAPys5ynL","gQvQ0qIoDC");
-$result=$conn->query($sql);
-        echo("<table border=1>");
-        echo("<th>id</th>");
-        echo("<th>imie</th>");
-        echo("<th>dzial</th>");
-        echo("<th>zarobki</th>");
-            while($row=$result->fetch_assoc()) {
-                echo("<tr>");
-                    echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td>");
-                echo("</tr>");
-            }
-        echo("</table>");
-echo("<h2>Zadanie 5</h2>");
-$sql = ("SELECT * FROM pracownicy where (imie like '%a') and (dzial = 1)");
-echo("<h2>".$sql."</h2>");
-$conn = new mysqli("remotemysql.com","gQvQ0qIoDC","4HAPys5ynL","gQvQ0qIoDC");
-$result=$conn->query($sql);
-        echo("<table border=1>");
-        echo("<th>id</th>");
-        echo("<th>imie</th>");
-        echo("<th>dzial</th>");
-        echo("<th>zarobki</th>");
-            while($row=$result->fetch_assoc()) {
-                echo("<tr>");
-                    echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td>");
-                echo("</tr>");
-            }
-        echo("</table>");
-?>
-</body>
+        <head>
+        <link rel="stylesheet" href="style.css">
+        </head>
+        <?php
+                echo("<h1>Emilia Kaiser</h1>");
+                echo("<h2>Zadanie 1</h2>");
+                $sql = ("SELECT * FROM pracownicy");
+                echo("<h2>".$sql."</h2>");
+                $conn = new mysqli("remotemysql.com","gQvQ0qIoDC","4HAPys5ynL","gQvQ0qIoDC");
+                $result=$conn->query($sql);
+                include("connect.php");
+                        echo("<table border=1>");
+                        echo("<th>id</th>");
+                        echo("<th>imie</th>");
+                        echo("<th>dzial</th>");
+                        echo("<th>zarobki</th>");
+                            while($row=$result->fetch_assoc()) {
+                                echo("<tr>");
+                                    echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td>");
+                                echo("</tr>");
+                            }
+                        echo("</table>");
+                echo("<h2>Zadanie 2</h2>");
+                $sql = ("SELECT * FROM pracownicy where imie like '%a'");
+                echo("<h2>".$sql."</h2>");
+                $conn = new mysqli("remotemysql.com","gQvQ0qIoDC","4HAPys5ynL","gQvQ0qIoDC");
+                $result=$conn->query($sql);
+                        echo("<table border=1>");
+                        echo("<th>id</th>");
+                        echo("<th>imie</th>");
+                        echo("<th>dzial</th>");
+                        echo("<th>zarobki</th>");
+                            while($row=$result->fetch_assoc()) {
+                                echo("<tr>");
+                                    echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td>");
+                                echo("</tr>");
+                            }
+                        echo("</table>");
+                echo("<h2>Zadanie 3</h2>");
+                $sql = ("SELECT * FROM pracownicy where (imie like '%a') and (dzial = 2)");
+                echo("<h2>".$sql."</h2>");
+                $conn = new mysqli("remotemysql.com","gQvQ0qIoDC","4HAPys5ynL","gQvQ0qIoDC");
+                $result=$conn->query($sql);
+                        echo("<table border=1>");
+                        echo("<th>id</th>");
+                        echo("<th>imie</th>");
+                        echo("<th>dzial</th>");
+                        echo("<th>zarobki</th>");
+                            while($row=$result->fetch_assoc()) {
+                                echo("<tr>");
+                                    echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td>");
+                                echo("</tr>");
+                            }
+                        echo("</table>");
+                echo("<h2>Zadanie 4</h2>");
+                $sql = ("SELECT * FROM pracownicy where (imie like '%a') and (dzial = 2 or dzial =3)");
+                echo("<h2>".$sql."</h2>");
+                $conn = new mysqli("remotemysql.com","gQvQ0qIoDC","4HAPys5ynL","gQvQ0qIoDC");
+                $result=$conn->query($sql);
+                        echo("<table border=1>");
+                        echo("<th>id</th>");
+                        echo("<th>imie</th>");
+                        echo("<th>dzial</th>");
+                        echo("<th>zarobki</th>");
+                            while($row=$result->fetch_assoc()) {
+                                echo("<tr>");
+                                    echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td>");
+                                echo("</tr>");
+                            }
+                        echo("</table>");
+                echo("<h2>Zadanie 5</h2>");
+                $sql = ("SELECT * FROM pracownicy where (imie like '%a') and (dzial = 1)");
+                echo("<h2>".$sql."</h2>");
+                $conn = new mysqli("remotemysql.com","gQvQ0qIoDC","4HAPys5ynL","gQvQ0qIoDC");
+                $result=$conn->query($sql);
+                        echo("<table border=1>");
+                        echo("<th>id</th>");
+                        echo("<th>imie</th>");
+                        echo("<th>dzial</th>");
+                        echo("<th>zarobki</th>");
+                            while($row=$result->fetch_assoc()) {
+                                echo("<tr>");
+                                    echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td>");
+                                echo("</tr>");
+                            }
+                        echo("</table>");
+        ?>
 </html>
