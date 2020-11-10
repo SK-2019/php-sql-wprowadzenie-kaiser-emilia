182c49f47654324e52c554b8d26a4a28d4decadd
kaiser-emilia <73523733+kaiser-emilia@users.noreply.github.com>
2020-11-10 10:01:46 +0100

    Create dataiczas.php

---
+++ dataiczas.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+        <head>
+        <link rel="stylesheet" href="style.css">
+        </head>
+            <div class="nav">
+        linki
+        <a href="https://yma.herokuapp.com/">Strona główna</a>
+        <a href="https://github.com/SK-2019/php-sql-wprowadzenie-kaiser-emilia">GITHUB</a>
+        <a href="pracownicy.php">Pracownicy</a>
+        <a href="pracownicyiorganizacja.php">Prac i Org</a>
+        <a href="dataiczas.php">Data i Czas</a>
+
+              </div>
+
+
+        <?php
+
+                echo("<h2>Zadanie 1</h2>");
+                $sql = ("SELECT * ,YEAR(curdate())-YEAR(data_urodzenia) AS wiek FROM pracownicy;");
+                echo("<h2>".$sql."</h2>");
+                $conn = new mysqli("remotemysql.com","gQvQ0qIoDC","4HAPys5ynL","gQvQ0qIoDC");
+                $result=$conn->query($sql);
+                include("connect.php");
+                        echo("<table border=1>");
+                        echo("<th>id_pracownicy</th>");
+                        echo("<th>imie</th>");
+                        echo("<th>dzial</th>");
+                        echo("<th>zarobki</th>");
+                        echo("<th>data_urodzenia</th>");
+                        echo("<th>wiek</th>");
+
+                        while($row=$result->fetch_assoc()) {
+                                echo("<tr>");
+                                    echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td><td>".$row["data_urodzenia"]."</td><td>".$row["wiek"]."</td>");
+                                echo("</tr>");
+                            }
+                        echo("</table>");
+
+        ?>
+</html>
