2580f9a35387e35698d63976fda8a706de3b9e83
kaiser-emilia <73523733+kaiser-emilia@users.noreply.github.com>
2020-11-13 11:19:28 +0100

    Update dataiczas.php

--- dataiczas.php
+++ dataiczas.php
@@ -259,7 +259,7 @@
 
                 echo("<h1>Formatowanie dat </h1>");
 
-                echo("<h1>Zadanie 1 Wyświetl nazwy dni w dacie urodzenia </h1>");
+                echo("<h1>Zadanie 1 - Wyświetl nazwy dni w dacie urodzenia </h1>");
                 $sql = ("SELECT *, DATE_FORMAT(data_urodzenia,'%W-%m-%Y') as format from pracownicy");
                 echo("<h2>".$sql."</h2>");
                 $conn = new mysqli("remotemysql.com","gQvQ0qIoDC","4HAPys5ynL","gQvQ0qIoDC");
@@ -281,6 +281,27 @@
                             }
                         echo("</table>");
 
+
+
+                echo("<h1>Zadanie 2 - Wypisz dzisiejszą nazwę dnia po polsku (np. poniedziałek) </h1>");
+                $sql1 = ("SET lc_time_names = 'pl_PL'");
+                $sql2 = ("SELECT DATE_FORMAT(CURDATE(), '%W')as data");
+                echo("<h2>".$sql1."</h2>");
+                $conn = new mysqli("remotemysql.com","gQvQ0qIoDC","4HAPys5ynL","gQvQ0qIoDC");
+                $result=$conn->query($sql1);
+                $result=$conn->query($sql2);
+
+                include("connect.php");
+                        echo("<table border=1>");
+                        echo("<th>data</th>");
+
+
+                        while($row=$result->fetch_assoc()) {
+                                echo("<tr>");
+                                    echo("<td>".$row["data"]."</td>");
+                                echo("</tr>");
+                            }
+                        echo("</table>");
 
 
         ?>
