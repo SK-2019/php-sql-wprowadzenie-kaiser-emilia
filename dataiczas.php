ed39a070e5522be2871ed3767eace12456e03e7f
kaiser-emilia <73523733+kaiser-emilia@users.noreply.github.com>
2020-11-13 15:08:53 +0100

    Update dataiczas.php

--- dataiczas.php
+++ dataiczas.php
@@ -451,6 +451,28 @@
 
 
 
+                echo("<h1>Zadanie 9 - Ilu pracowników urodziło się w poniedziałek </h1>");
+                $sql1 = ("SET lc_time_names = 'pl_PL'");
+                $sql2 = ("SELECT Count(DATE_FORMAT(data_urodzenia,'%W') as urodzeniwpon  where DATE_FORMAT(data_urodzenia, '%W')='Poniedziałek'");
+                echo("<h2>".$sql2."</h2>");
+                $conn = new mysqli("remotemysql.com","gQvQ0qIoDC","4HAPys5ynL","gQvQ0qIoDC");
+                $result=$conn->query($sql1);
+                $result=$conn->query($sql2);
+                include("connect.php");
+                        echo("<table border=1>");
+                        echo("<th>urodzeniwpon</th>");
+
+
+                        while($row=$result->fetch_assoc()) {
+                                echo("<tr>");
+                                    echo("<td>".$row["urodzeniwpon"]."</td>");
+                                echo("</tr>");
+                            }
+                        echo("</table>");
+
+
+
+
 
 
         ?>
