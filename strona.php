3c3f25c63cff5efb83172788b6e22fb11835263c
kaiser-emilia <73523733+kaiser-emilia@users.noreply.github.com>
2020-11-17 10:10:50 +0100

    Update strona.php

--- strona.php
+++ strona.php
@@ -6,11 +6,11 @@
 </html> 
   
 <?php
-ul
-echo("<li> imie: ".$_POST['firstname']."<li>");
-echo($_POST['name']);
-echo($_POST['phone']);
-echo($_POST['city']);
+
+echo("<li> imie: ".$_POST['firstname']."</li>");
+echo("<li> nazwisko: ".$_POST['name']."</li>");
+echo("<li> numer tel: ".$_POST['phone']."</li>");
+echo("<li> miasto: ".$_POST['city']."</li>");
 
 
 ?>
