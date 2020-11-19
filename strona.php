67b7674f2740f030416a6229733aa06eaad9feb7
kaiser-emilia <73523733+kaiser-emilia@users.noreply.github.com>
2020-11-19 11:21:53 +0100

    Update strona.php

--- strona.php
+++ strona.php
@@ -8,7 +8,7 @@
 <?php
 include("connect.php");
 echo("<li> imie: ".$_POST['firstname']."</li>");
-echo("<li> nazwisko: ".$_POST['name']."</li>");
+echo("<li> nazwisko: ".$_POST['lastname']."</li>");
 echo("<li> miasto: ".$_POST['city']."</li>");
 echo("<li> numer tel: ".$_POST['phone']."</li>");
 echo("<li> kod pocztowy: ".$_POST['postcode']."</li>");
