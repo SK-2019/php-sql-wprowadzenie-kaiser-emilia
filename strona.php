965fc78c722552ab474c5e5a403a1da5786391c3
kaiser-emilia <73523733+kaiser-emilia@users.noreply.github.com>
2020-11-19 11:26:29 +0100

    Update strona.php

--- strona.php
+++ strona.php
@@ -11,7 +11,7 @@ echo("<li> imie: ".$_POST['firstname']."</li>");
 echo("<li> nazwisko: ".$_POST['lastname']."</li>");
 echo("<li> miasto: ".$_POST['city']."</li>");
 echo("<li> numer tel: ".$_POST['phone']."</li>");
-echo("<li> kod pocztowy: ".$_POST['postcode']."</li>");
+echo("<li> kod pocztowy: ".$_POST['poscode']."</li>");
 
 
 $sql="INSERT INTO 'pracownicy'('id', 'imie', 'dzial', 'zarobki', 'data_urodzenia') VALUES (null,'józef',2,23,'2001-10-23')";
