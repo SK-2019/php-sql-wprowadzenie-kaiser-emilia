5d339cda4b330654df16dd34f7f686be9a5dd432
kaiser-emilia <73523733+kaiser-emilia@users.noreply.github.com>
2020-11-20 16:11:11 +0100

    Update index.php

--- index.php
+++ index.php
@@ -23,7 +23,7 @@
         linki
         <a href="https://yma.herokuapp.com/">Strona główna</a>  |
         <a href="https://github.com/SK-2019/php-sql-wprowadzenie-kaiser-emilia">GITHUB</a>  |
-        <a href="pracownicy.php">Pracownicy</a>  |
+        <a href="pracownicy.php"><img class="obrazek" src="aard.png">Pracownicy</a>  |
         <a href="pracownicyiorganizacja.php">Prac i Org</a>  |
         <a href="agregat.php">Funkcje Agregujace</a>  |
         <a href="dataiczas.php">Data i Czas</a>  |
