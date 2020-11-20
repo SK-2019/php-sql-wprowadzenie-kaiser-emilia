369d2a80151ca0648bc759c43fdbfd95a95bf62a
kaiser-emilia <73523733+kaiser-emilia@users.noreply.github.com>
2020-11-20 14:46:47 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -10,7 +10,7 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "INSERT INTO pracownicy (null, imie, dzial, zarobki, data_urodzenia) 
+$sql = "INSERT INTO pracownicy (id_pracownicy, imie, dzial, zarobki, data_urodzenia) 
        VALUES (null,'Ksawery', 3, 36,'1995-10-21')";
 
 $conn->query($sql);
