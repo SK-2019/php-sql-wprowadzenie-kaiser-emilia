47ff553101c0c4663c16a5fc141e4dbfbeb76f82
kaiser-emilia <73523733+kaiser-emilia@users.noreply.github.com>
2020-11-20 11:41:56 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -9,27 +9,27 @@
 
 
             
-//     $sql = "INSERT INTO pracownicy (null, name, dzial,zarobki,data_urodzenia)
-//             VALUES (
-//               null, 
-//               $_POST['name'], 
-//               $_POST['dzial'], 
-//               $_POST['zarobki'],
-//               $_POST['data_urodzenia']
-//             )";
+    $sql = "INSERT INTO pracownicy (null, name, dzial,zarobki,data_urodzenia)
+            VALUES (
+              null, 
+              $_POST['name'], 
+              $_POST['dzial'], 
+              $_POST['zarobki'],
+              $_POST['data_urodzenia']
+            )";
 
-//  $conn = new mysqli("remotemysql.com","gQvQ0qIoDC","4HAPys5ynL","gQvQ0qIoDC");
-//                 $result=$conn->query($sql);
-//                 include("connect.php"); 
+ $conn = new mysqli("remotemysql.com","gQvQ0qIoDC","4HAPys5ynL","gQvQ0qIoDC");
+                $result=$conn->query($sql);
+                include("connect.php"); 
 
 
-//       echo "<li>". $sql;
+      echo ($sql);
 
-//       if ($conn->query($sql) === TRUE) {
-//         echo "New record created successfully";
-//       } else {
-//         echo "Error: " . $sql . "<br>" . $conn->error;
-//       }
+      if ($conn->query($sql) === TRUE) {
+        echo "New record created successfully";
+      } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+      }
 
-//       $conn->close();
-//       ?>
+      $conn->close();
+      ?>
