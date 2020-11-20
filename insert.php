c7356689814815b96f721e1a15c11da09f8b8df6
kaiser-emilia <73523733+kaiser-emilia@users.noreply.github.com>
2020-11-20 11:43:16 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -23,13 +23,13 @@
                 include("connect.php"); 
 
 
-      echo ($sql);
+//       echo ($sql);
 
-      if ($conn->query($sql) === TRUE) {
-        echo "New record created successfully";
-      } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
-      }
+//       if ($conn->query($sql) === TRUE) {
+//         echo "New record created successfully";
+//       } else {
+//         echo "Error: " . $sql . "<br>" . $conn->error;
+//       }
 
-      $conn->close();
+//       $conn->close();
       ?>
