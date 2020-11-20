5a8cf6963a807a6d6cd945f502990f180ffa7ede
kaiser-emilia <73523733+kaiser-emilia@users.noreply.github.com>
2020-11-20 15:12:50 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -24,7 +24,7 @@ $sql = "INSERT INTO pracownicy (id_pracownicy, imie, dzial,zarobki,data_urodzeni
 					$_POST['name'], 
 					$_POST['dzial'], 
 					$_POST['zarobki'],
-					$_POST['data_urodzenia'])"
+					$_POST['data_urodzenia'])
 
 
 echo ("<li>". $sql);
