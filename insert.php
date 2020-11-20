ca54363afa0f980ca07eb6c4577ed3b4bbc8aee5
kaiser-emilia <73523733+kaiser-emilia@users.noreply.github.com>
2020-11-20 11:40:25 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -9,18 +9,18 @@
 
 
             
-    $sql = "INSERT INTO pracownicy (null, name, dzial,zarobki,data_urodzenia)
-            VALUES (
-              null, 
-              $_POST['name'], 
-              $_POST['dzial'], 
-              $_POST['zarobki'],
-              $_POST['data_urodzenia']
-            )";
+//     $sql = "INSERT INTO pracownicy (null, name, dzial,zarobki,data_urodzenia)
+//             VALUES (
+//               null, 
+//               $_POST['name'], 
+//               $_POST['dzial'], 
+//               $_POST['zarobki'],
+//               $_POST['data_urodzenia']
+//             )";
 
- $conn = new mysqli("remotemysql.com","gQvQ0qIoDC","4HAPys5ynL","gQvQ0qIoDC");
-                $result=$conn->query($sql);
-                include("connect.php"); 
+//  $conn = new mysqli("remotemysql.com","gQvQ0qIoDC","4HAPys5ynL","gQvQ0qIoDC");
+//                 $result=$conn->query($sql);
+//                 include("connect.php"); 
 
 
 //       echo "<li>". $sql;
