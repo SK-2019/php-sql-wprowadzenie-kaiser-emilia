67a82f407483f4a8c1f5a06984656beabb858b64
kaiser-emilia <73523733+kaiser-emilia@users.noreply.github.com>
2020-11-20 15:35:21 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -27,7 +27,7 @@ $sql = "INSERT INTO pracownicy (id_pracownicy, imie, dzial,zarobki,data_urodzeni
 					'.$_POST['data_urodzenia'].');
 
 
-echo ("<li>". $sql);
+echo "<li>". $sql;
 
 if ($conn->query($sql) === TRUE) {
   echo ("New record created successfully");
