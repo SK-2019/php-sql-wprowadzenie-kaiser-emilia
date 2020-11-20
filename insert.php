ffbd3230daf8c0db970cf76dbc8c320a86522e5e
kaiser-emilia <73523733+kaiser-emilia@users.noreply.github.com>
2020-11-20 11:39:05 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -2,14 +2,14 @@
 
     <?php
     echo("jestes w insert.php <br>");
-    echo "<li>". $_POST['name'];
-    echo "<li>". $_POST['dzial'];
-    echo "<li>". $_POST['zarobki'];
-    echo "<li>". $_POST['data_urodzenia'];
+    echo ("<li>". $_POST['name']);
+    echo ("<li>". $_POST['dzial']);
+    echo ("<li>". $_POST['zarobki']);
+    echo ("<li>". $_POST['data_urodzenia']);
 
 
             
-    $sql = "INSERT INTO pracownicy (id, name, dzial,zarobki,data_urodzenia)
+    $sql = "INSERT INTO pracownicy (null, name, dzial,zarobki,data_urodzenia)
             VALUES (
               null, 
               $_POST['name'], 
