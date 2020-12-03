581eeb1c030ae4306387ebb6218203e5d721b66e
kaiser-emilia <73523733+kaiser-emilia@users.noreply.github.com>
2020-12-03 11:11:09 +0100

    Update ksiazki.php

--- ksiazki.php
+++ ksiazki.php
@@ -44,7 +44,7 @@ $result=$conn->query($sql);
 
         while($row=$result->fetch_assoc()) {
                 echo("<tr>");
-                    <option value=.$row["tytul"].>.$row["tytul"].</option>;
+                    echo("<option value=.$row["tytul"].>.$row["tytul"].</option>");
                 echo("</tr>");
             }
         echo("</table>");
