b3b2077c62aa33306552956ee5cfb2a3bc18b21c
kaiser-emilia <73523733+kaiser-emilia@users.noreply.github.com>
2021-02-23 10:28:29 +0100

    Update connect.php

--- assets/connect.php
+++ assets/connect.php
@@ -1,8 +1,5 @@
 <?php
-$servername = "remotemysql.com";
-$username = "gQvQ0qIoDC";
-$password = "4HAPys5ynL";
-$dbname = "gQvQ0qIoDC";
+
 
 $conn=new mysqli($servername, $username, $password, $dbname);
         if($conn->connect_error) {
