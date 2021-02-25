f7362513a291d11256768bb16a4248d654b9672f
kaiser-emilia <73523733+kaiser-emilia@users.noreply.github.com>
2021-02-25 11:20:47 +0100

    Update connect.php

--- assets/connect.php
+++ assets/connect.php
@@ -1,7 +1,7 @@
 <?php
 
 
-$conn=new mysqli($servername, $username, $password, $dbname);
+$conn=new mysqli($_SERVER['servername'], $_SERVER['username'], $_SERVER['password'], $_SERVER['dbname']);
         if($conn->connect_error) {
             die("Connection failed: " . $conn->connect_error);
         }
