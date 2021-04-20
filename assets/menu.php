f7f48339d0be4c72892f9f1b9a1e267fb3fa7df0
kaiser-emilia <73523733+kaiser-emilia@users.noreply.github.com>
2021-04-20 09:40:32 +0200

    Add files via upload

---
+++ assets/menu.php
@@ -0,0 +1,22 @@
+
+
+
+        <a class="link b" href="https://github.com/SK-2019/php-sql-wprowadzenie-kaiser-emilia"><img class="obrazek" src="/inne/aksji.png">GITHUB<img class="obrazek" src="/inne/aksji.png"></a>  
+        <a class="link c" href="/pracownicy-organizacja/pracownicy.php"><img class="obrazek" src="/inne/aard.png"> Pracownicy <img class="obrazek" src="/inne/aard.png"></a>  
+        <a class="link d" href="/pracownicy-organizacja/pracownicyiorganizacja.php"><img class="obrazek" src="/inne/quen.png">Prac i Org<img class="obrazek" src="/inne/quen.png"></a>  
+        <a class="link e" href="/pracownicy-organizacja/agregat.php"><img class="obrazek" src="/inne/yrden.png">Funkcje Agregujace<img class="obrazek" src="/inne/yrden.png"></a>  
+        <a class="link f" href="/pracownicy-organizacja/dataiczas.php"><img class="obrazek" src="/inne/igni.png">Data i Czas<img class="obrazek" src="inne/igni.png"></a>  
+        <a class="link g" href="/dane-do-bazy/formularz.html"><img class="obrazek" src="/inne/aksji.png">Formularz<img class="obrazek" src="/inne/aksji.png"></a>  
+        <a class="link h" href="/dane-do-bazy/daneDoBazy.php"><img class="obrazek" src="/inne/aard.png">Dane do bazy<img class="obrazek" src="/inne/aard.png"></a>
+        <a class="link i" href="/biblioteka/ksiazki.php"><img class="obrazek" src="/inne/quen.png">Książki<img class="obrazek" src="/inne/quen.png"></a>
+        <a class="link j" href="/flexbox/index.html"><img class="obrazek" src="/inne/yrden.png">Flexbox<img class="obrazek" src="/inne/yrden.png"></a>
+        <a class="link k" href="/biblioteka/wieledowielu.php"><img class="obrazek" src="/inne/igni.png">Wiele do wielu<img class="obrazek" src="/inne/igni.png"></a>
+        <a class="link l" href="/biblioteka/autor-tytul.php"><img class="obrazek" src="/inne/aksji.png">autor - tytuł<img class="obrazek" src="/inne/aksji.png"></a>
+        <a class="link m" href="/biblioteka/nauczyciel-klasa.php"><img class="obrazek" src="/inne/aard.png">Wiele do wielu<img class="obrazek" src="/inne/aard.png"></a>
+        <a class="link n" href="/biblioteka/lekarz-pacjent.php"><img class="obrazek" src="/inne/quen.png">Wiele do wielu<img class="obrazek" src="/inne/quen.png"></a>
+
+
+
+
+   
+
