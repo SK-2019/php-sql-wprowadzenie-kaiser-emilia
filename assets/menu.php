660d0700f676b401e4988413828bbd4541167936
kaiser-emilia <73523733+kaiser-emilia@users.noreply.github.com>
2021-04-24 21:50:51 +0200

    Add files via upload

--- assets/menu.php
+++ assets/menu.php
@@ -1,23 +1,39 @@
 
 
 
+  
+
+
+
+        <div class="nav">
         <a class="link a" href="https://github.com/SK-2019/php-sql-wprowadzenie-kaiser-emilia"><img class="obrazek" src="/inne/igni.png">GITHUB<img class="obrazek" src="/inne/igni.png"></a>
-        <a class="link b" href="/index.php"><img class="obrazek" src="/inne/aksji.png">Strona główna<img class="obrazek" src="/inne/aksji.png"></a>    
-        <a class="link c" href="/pracownicy-organizacja/pracownicy.php"><img class="obrazek" src="/inne/aard.png"> Pracownicy <img class="obrazek" src="/inne/aard.png"></a>  
-        <a class="link d" href="/pracownicy-organizacja/pracownicyiorganizacja.php"><img class="obrazek" src="/inne/quen.png">Prac i Org<img class="obrazek" src="/inne/quen.png"></a>  
-        <a class="link e" href="/pracownicy-organizacja/agregat.php"><img class="obrazek" src="/inne/yrden.png">Funkcje Agregujace<img class="obrazek" src="/inne/yrden.png"></a>  
-        <a class="link f" href="/pracownicy-organizacja/dataiczas.php"><img class="obrazek" src="/inne/igni.png">Data i Czas<img class="obrazek" src="/inne/igni.png"></a>  
-        <a class="link g" href="/dane-do-bazy/formularz.php"><img class="obrazek" src="/inne/aksji.png">Formularz<img class="obrazek" src="/inne/aksji.png"></a>  
-        <a class="link h" href="/dane-do-bazy/daneDoBazy.php"><img class="obrazek" src="/inne/aard.png">Dane do bazy<img class="obrazek" src="/inne/aard.png"></a>
-        <a class="link i" href="/biblioteka/ksiazki.php"><img class="obrazek" src="/inne/quen.png">Książki<img class="obrazek" src="/inne/quen.png"></a>
-        <a class="link j" href="/flexbox/index.html"><img class="obrazek" src="/inne/yrden.png">Flexbox<img class="obrazek" src="/inne/yrden.png"></a>
-        <a class="link k" href="/biblioteka/wieledowielu.php"><img class="obrazek" src="/inne/igni.png">Wiele do wielu<img class="obrazek" src="/inne/igni.png"></a>
-        <a class="link l" href="/biblioteka/autor-tytul.php"><img class="obrazek" src="/inne/aksji.png">autor - tytuł<img class="obrazek" src="/inne/aksji.png"></a>
-        <a class="link m" href="/biblioteka/nauczyciel-klasa.php"><img class="obrazek" src="/inne/aard.png">Wiele do wielu<img class="obrazek" src="/inne/aard.png"></a>
-        <a class="link n" href="/biblioteka/lekarz-pacjent.php"><img class="obrazek" src="/inne/quen.png">Wiele do wielu<img class="obrazek" src="/inne/quen.png"></a>
-
-
-
-
-   
+        <a class="link b" href="/index.php"><img class="obrazek" src="/inne/aksji.png">Strona główna<img class="obrazek" src="/inne/aksji.png"></a>
+  <div class="subnav">
+    <button class="link c">Pracownicy i organizacja <i class="fa fa-caret-down"></i></button>
+    <div class="subnav-content">
+      <a href="/pracownicy-organizacja/pracownicy.php">Pracownicy</a>
+      <a href="/pracownicy-organizacja/pracownicyiorganizacja.php">PracOrg</a>
+      <a href="/pracownicy-organizacja/agregat.php">Funkcje agregujące</a>
+      <a href="/pracownicy-organizacja/dataiczas.php">Data i czas</a>
+    </div>
+  </div>
+  <div class="subnav">
+    <button class="link d">Dane do bazy <i class="fa fa-caret-down"></i></button>
+    <div class="subnav-content">
+      <a href="/dane-do-bazy/formularz.php">Formularz</a>
+      <a href="/dane-do-bazy/daneDoBazy.php">Dane do bazy</a>
+    </div>
+  </div>
+  <a class="link i" href="/biblioteka/ksiazki.php"><img class="obrazek" src="/inne/quen.png">Książki<img class="obrazek" src="/inne/quen.png"></a>
+  <a class="link j" href="/flexbox/index.html"><img class="obrazek" src="/inne/yrden.png">Flexbox<img class="obrazek" src="/inne/yrden.png"></a>
+  <div class="subnav">
+    <button class="link e">Relacje <i class="fa fa-caret-down"></i></button>
+    <div class="subnav-content">
+      <a href="/relacje/autor-tytul.php">autor-tytuł</a>
+      <a href="/relacje/nauczyciel-klasa.php">nauczyciel-klasa</a>
+      <a href="/relacje/lekarz-pacjent.php">lekarz-pacjent.php</a>
+      <a href="#link4">Link 4</a>
+    </div>
+  </div>
+</div>
 
