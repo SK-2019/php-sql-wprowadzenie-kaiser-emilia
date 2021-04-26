126aaa94a176ab0ea4f438e69fdc50544fa60f02
kaiser-emilia <73523733+kaiser-emilia@users.noreply.github.com>
2021-04-27 00:23:27 +0200

    Update przychodnia.php

--- relacje/przychodnia.php
+++ relacje/przychodnia.php
@@ -9,7 +9,7 @@
 
 <style>
         body{
-            background-image:url(https://preview.redd.it/qb2jre5sfmx01.png?auto=webp&s=11cb1b966f18f8c81356ed18be5f18c68a95c764);          
+            background-image:url(https://images4.alphacoders.com/711/thumb-1920-711124.jpg);          
             background-repeat: no-repeat;
           background-attachment: fixed;
           background-size: cover;
